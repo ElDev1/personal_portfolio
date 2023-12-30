b1bc79b1dabef060c05ea4b90f9d0ce3be86586d
Devi Amaolo <deviamaolo@gmail.com>
2023-12-30 00:53:49 -0300

    project setting

---
+++ src/components/Nav.jsx
@@ -0,0 +1,32 @@
+function Nav () {
+  return (
+    <nav>
+      <div>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Web Projects</a>
+          </li>
+          <li>
+            <a href="">Data Projects</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+        <div>
+          <a href="">
+            
+          </a>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
